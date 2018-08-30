68985e8b98fa379254d36ffcdde941f025badba8
Ethan Schaffer <ethan.schaffer@data.world>
2018-08-30 13:28:05 -0400

    Euler 39

--- src/Euler39/Euler39.scala
+++ src/Euler39/Euler39.scala
@@ -1,3 +1,6 @@
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
+
 object Euler39 {
   def main(args: Array[String]): Unit = {
     /*
@@ -8,7 +11,36 @@ object Euler39 {
     Written by Ethan Schaffer
      */
 
-    println("Hello World")
+    val largestNumber = 1000
+    var list: ListBuffer[Int] = new ListBuffer[Int]
+    for (i <- 1 to largestNumber) {
+      for (j <- i to largestNumber) {
+        val hypotenuse = getThirdSide(i, j)
+        if (hypotenuse != 0) {
+          if (i + j + hypotenuse < largestNumber) {
+            list += i + j + hypotenuse
+          }
+        }
+      }
+    }
+    val grouped = list.toList.groupBy(v => v)
+    val sorted = grouped.toList.sortBy(v => v._2.length).reverse
+    val solution = sorted.head._1
+    println(solution)
+
+  }
+
+  def getThirdSide(legA: Int, legB: Int): Int = {
+    val sqrt = Math.sqrt(legA * legA + legB * legB)
+    if (isInteger(sqrt)) {
+      sqrt.toInt
+    } else {
+      0
+    }
+  }
+
+  def isInteger(number: Double): Boolean = {
+    Math.ceil(number) == Math.floor(number);
   }
 
 }
